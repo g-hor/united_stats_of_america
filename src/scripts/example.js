18e94117c2bdea2b8e72d37d94de21066465b5e6
Gary <geehor@gmail.com>
2023-03-09 17:00:14 -0500

    Test change branch

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,7 +1,7 @@
 class Example {
   constructor(ele) {
       this.ele = ele
-      this.ele.innerHTML = "<h1>It's CAPY ALIVEEEE!!!</h1>"
+      this.ele.innerHTML = "<h1>Capy's ALIVE!!!</h1>"
 
       this.handleClick = this.handleClick.bind(this);
       this.ele.addEventListener("click", this.handleClick)
